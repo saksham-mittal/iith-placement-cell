66d1b63bc56f012224b97c56fc76b3afbed55110
saksham-mittal <mittalsaksham01@gmail.com>
2018-02-12 17:30:53 +0530

    Added ther pages too

--- placement/urls.py
+++ placement/urls.py
@@ -6,4 +6,11 @@ app_name = 'placement'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^announcements/', views.announcements, name='announcements'),
+    url(r'^internships/', views.internships, name='internships'),
+    url(r'^selectionProcess/', views.selectionProcess, name='selectionProcess'),
+    url(r'^studentLife/', views.studentLife, name='studentLife'),
+    url(r'^placementProcess/', views.placementProcess, name='placementProcess'),
+    url(r'^hotelsnearIITH/', views.hotelsnearIITH, name='hotelsnearIITH'),
+    url(r'^contactUs/', views.contactUs, name='contactUs'),
+    url(r'^credits/', views.credits, name='credits'),
 ]
--- placement/views.py
+++ placement/views.py
@@ -9,3 +9,24 @@ def index(request):
 def announcements(request):
     announcements = Announcement.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'placement/announcements.html', {'announcements' : announcements})
+
+def internships(request):
+    return render(request, 'placement/internships.html', {})
+
+def selectionProcess(request):
+    return render(request, 'placement/selectionProcess.html', {})
+
+def studentLife(request):
+    return render(request, 'placement/studentLife.html', {})
+
+def placementProcess(request):
+    return render(request, 'placement/placementProcess.html', {})
+
+def hotelsnearIITH(request):
+    return render(request, 'placement/hotelsnearIITH.html', {})
+
+def contactUs(request):
+    return render(request, 'placement/contactUs.html', {})
+
+def credits(request):
+    return render(request, 'placement/credits.html', {})
